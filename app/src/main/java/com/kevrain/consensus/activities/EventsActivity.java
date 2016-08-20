bb04314fc297a052183f7e0d7c5dc389d6053d1e
Kevin Farst <kevin.farst@zozi.com>
2016-08-20 13:18:39 -0700

    Add logout option

--- app/src/main/java/com/kevrain/consensus/activities/EventsActivity.java
+++ app/src/main/java/com/kevrain/consensus/activities/EventsActivity.java
@@ -1,16 +1,14 @@
 package com.kevrain.consensus.activities;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ListView;
 
+import com.facebook.login.LoginManager;
 import com.kevrain.consensus.R;
 import com.kevrain.consensus.adapter.EventsArrayAdapter;
 import com.kevrain.consensus.models.Events;
@@ -45,21 +43,7 @@ public class EventsActivity extends AppCompatActivity {
         adapter = new EventsArrayAdapter(this, events);
         lvEvents.setAdapter(adapter);
 
-
         //###### Populate data into events list view here
-
-
-        btnCreateEvent.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.d("Button click", "Buttton");
-                Intent i = new Intent(EventsActivity.this, CreateNewEventActivity.class);
-                startActivityForResult(i, REQUEST_CODE);
-            }
-        });
-
-
-
     }
 
     private void populateData() {
@@ -70,7 +54,7 @@ public class EventsActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_events, menu);
-        return true;
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
@@ -78,13 +62,19 @@ public class EventsActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch(item.getItemId()) {
+            case R.id.action_settings:
+                return true;
+            case R.id.action_logout:
+                logOut();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
+    }
 
-        return super.onOptionsItemSelected(item);
+    private void logOut() {
+        LoginManager.getInstance().logOut();
+        finish();
     }
 }
