382d8a6381c6d490d6adbca961a3788dcaff6528
Kevin Farst <kevin.farst@zozi.com>
2016-09-02 22:58:27 -0700

    wip

--- app/src/main/java/com/kevrain/consensus/adapter/PollsArrayAdapter.java
+++ app/src/main/java/com/kevrain/consensus/adapter/PollsArrayAdapter.java
@@ -1,13 +1,23 @@
 package com.kevrain.consensus.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.h6ah4i.android.widget.advrecyclerview.swipeable.SwipeableItemAdapter;
+import com.h6ah4i.android.widget.advrecyclerview.swipeable.SwipeableItemConstants;
+import com.h6ah4i.android.widget.advrecyclerview.swipeable.action.SwipeResultAction;
+import com.h6ah4i.android.widget.advrecyclerview.swipeable.action.SwipeResultActionDefault;
+import com.h6ah4i.android.widget.advrecyclerview.swipeable.action.SwipeResultActionMoveToSwipedDirection;
+import com.h6ah4i.android.widget.advrecyclerview.swipeable.action.SwipeResultActionRemoveItem;
+import com.h6ah4i.android.widget.advrecyclerview.utils.AbstractSwipeableItemViewHolder;
 import com.kevrain.consensus.R;
+import com.kevrain.consensus.activities.CreateOrEditPollActivity;
 import com.kevrain.consensus.models.Poll;
 import com.kevrain.consensus.models.PollOption;
 import com.parse.FindCallback;
@@ -21,19 +31,36 @@ import butterknife.ButterKnife;
 /**
  * Created by shravyagarlapati on 8/19/16.
  */
-public class PollsArrayAdapter extends RecyclerView.Adapter<PollsArrayAdapter.ViewHolder> {
+public class PollsArrayAdapter extends RecyclerView.Adapter<PollsArrayAdapter.ViewHolder> implements SwipeableItemAdapter<PollsArrayAdapter.ViewHolder> {
+    interface Swipeable extends SwipeableItemConstants {
+    }
+
+    static final float OPTIONS_AREA_PROPORTION = 0.3f;
+    static final float REMOVE_ITEM_THRESHOLD = 0.6f;
     public List<Poll> mPolls;
+    float touchPosition;
 
     // Pass in the contact array into the constructor
     public PollsArrayAdapter(List<Poll> locations) {
+        setHasStableIds(true);
         mPolls = locations;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
     }
 
     // Provide a direct reference to each of the views within a data item
     // Used to cache the views within the item layout for fast access
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    public class ViewHolder extends AbstractSwipeableItemViewHolder implements View.OnTouchListener {
         @BindView(R.id.tvPollName) TextView tvPollName;
         @BindView(R.id.tvPollOptionCount) TextView tvPollOptionCount;
+        @BindView(R.id.swipeableContainer) View swipeableContainer;
+        @BindView(R.id.option_view_1) View optionView1;
+        @BindView(R.id.option_view_2) View optionView2;
+
+        float lastSwipeAmount;
 
         public ViewHolder(View itemView) {
             // Stores the itemView in a public final member variable that can be used
@@ -41,6 +68,59 @@ public class PollsArrayAdapter extends RecyclerView.Adapter<PollsArrayAdapter.Vi
             super(itemView);
 
             ButterKnife.bind(this, itemView);
+        }
+
+        @Override
+        public View getSwipeableContainerView() {
+            return swipeableContainer;
+        }
+
+        @Override
+        public void onSlideAmountUpdated(float horizontalAmount, float verticalAmount, boolean isSwiping) {
+            int itemWidth = itemView.getWidth();
+            float optionItemWidth = itemWidth * OPTIONS_AREA_PROPORTION / 2;
+            int offset = (int) (optionItemWidth + 0.5f);
+            float p = Math.max(0, Math.min(OPTIONS_AREA_PROPORTION, -horizontalAmount)) / OPTIONS_AREA_PROPORTION;
+
+            if (optionView1.getWidth() == 0) {
+                setLayoutWidth(optionView1, (int) (optionItemWidth + 0.5f));
+                setLayoutWidth(optionView2, (int) (optionItemWidth + 0.5f));
+            }
+
+            optionView1.setTranslationX(-(int) (p * optionItemWidth * 2 + 0.5f) + offset);
+            optionView2.setTranslationX(-(int) (p * optionItemWidth * 1 + 0.5f) + offset);
+
+            swipeableContainer.setVisibility(View.VISIBLE);
+            optionView1.setVisibility(View.VISIBLE);
+            optionView2.setVisibility(View.VISIBLE);
+
+            lastSwipeAmount = horizontalAmount;
+        }
+
+        private void setLayoutWidth(View v, int width) {
+            ViewGroup.LayoutParams lp = v.getLayoutParams();
+            lp.width = width;
+            v.setLayoutParams(lp);
+        }
+
+        @Override
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+
+            switch (motionEvent.getAction()) {
+                case MotionEvent.ACTION_DOWN: {
+                   touchPosition = motionEvent.getY();
+                }
+                case MotionEvent.ACTION_UP: {
+                    if (touchPosition == motionEvent.getY()) {
+                        Intent i = new Intent(view.getContext(), CreateOrEditPollActivity.class);
+                        i.putExtra("pollID", mPolls.get(getAdapterPosition()).getObjectId());
+                        i.putExtra("groupID", mPolls.get(getAdapterPosition()).getGroup().getObjectId());
+                        view.getContext().startActivity(i);
+                    }
+                    touchPosition = Float.parseFloat(null);
+                }
+            }
+            return true;
         }
     }
 
@@ -60,7 +140,7 @@ public class PollsArrayAdapter extends RecyclerView.Adapter<PollsArrayAdapter.Vi
     }
 
     @Override
-    public void onBindViewHolder(final PollsArrayAdapter.ViewHolder holder, final int position) {
+    public void onBindViewHolder(final ViewHolder holder, final int position) {
 
         final Poll poll = mPolls.get(position);
 
@@ -74,10 +154,85 @@ public class PollsArrayAdapter extends RecyclerView.Adapter<PollsArrayAdapter.Vi
                 }
             }
         });
+
+        // set swiping properties
+        holder.setMaxLeftSwipeAmount(-OPTIONS_AREA_PROPORTION);
+        holder.setMaxRightSwipeAmount(0);
+        holder.setSwipeItemHorizontalSlideAmount(poll.pinned ? -OPTIONS_AREA_PROPORTION : 0);
     }
 
     @Override
     public int getItemCount() {
         return mPolls.size();
+    }
+
+    @Override
+    public SwipeResultAction onSwipeItem(ViewHolder holder, int position, int result) {
+        if (result == Swipeable.RESULT_SWIPED_LEFT) {
+            return new SwipeLeftPinningAction(this, position);
+        } else {
+            return new SwipeCancelAction(this, position);
+        }
+    }
+
+    @Override
+    public int onGetSwipeReactionType(ViewHolder holder, int position, int x, int y) {
+        return Swipeable.REACTION_CAN_SWIPE_LEFT;
+    }
+
+    @Override
+    public void onSetSwipeBackground(ViewHolder holder, int position, int type) {
+        if (type == Swipeable.DRAWABLE_SWIPE_LEFT_BACKGROUND) {
+            holder.itemView.setBackgroundColor(0xffff6666);
+        }
+    }
+
+    static class SwipeLeftRemoveAction extends SwipeResultActionRemoveItem {
+        PollsArrayAdapter adapter;
+        int position;
+
+        public SwipeLeftRemoveAction(PollsArrayAdapter adapter, int position) {
+            this.adapter = adapter;
+            this.position = position;
+        }
+
+        @Override
+        protected void onPerformAction() {
+            adapter.mPolls.remove(position);
+            adapter.notifyItemRemoved(position);
+        }
+    }
+
+    static class SwipeLeftPinningAction extends SwipeResultActionMoveToSwipedDirection {
+        PollsArrayAdapter adapter;
+        int position;
+
+        public SwipeLeftPinningAction(PollsArrayAdapter adapter, int position) {
+            this.adapter = adapter;
+            this.position = position;
+        }
+
+        @Override
+        protected void onPerformAction() {
+            adapter.mPolls.get(position).pinned = true;
+            adapter.notifyItemChanged(position);
+        }
+    }
+
+
+    static class SwipeCancelAction extends SwipeResultActionDefault {
+        PollsArrayAdapter adapter;
+        int position;
+
+        public SwipeCancelAction(PollsArrayAdapter adapter, int position) {
+            this.adapter = adapter;
+            this.position = position;
+        }
+
+        @Override
+        protected void onPerformAction() {
+            adapter.mPolls.get(position).pinned = false;
+            adapter.notifyItemChanged(position);
+        }
     }
 }
