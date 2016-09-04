bafaac4e0cc6a563ad9822aa780175964785bfb1
Iris Wang <iris@pinterest.com>
2016-09-03 19:21:31 -0700

    altered group view to be a card view instead

--- app/src/main/java/com/kevrain/consensus/activities/GroupsActivity.java
+++ app/src/main/java/com/kevrain/consensus/activities/GroupsActivity.java
@@ -7,6 +7,7 @@ import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
@@ -61,7 +62,8 @@ public class GroupsActivity extends AppCompatActivity {
         adapter = new GroupsArrayAdapter(groups);
         rvGroups.setAdapter(adapter);
 
-        rvGroups.setLayoutManager(new LinearLayoutManager(this));
+        final GridLayoutManager layout = new GridLayoutManager(GroupsActivity.this, 2);
+        rvGroups.setLayoutManager(layout);
         populateGroups();
         fabAddGroup.setOnClickListener(new View.OnClickListener() {
             @Override
